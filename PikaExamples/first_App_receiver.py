aeb1e2bf7ea55805912a528182a26b2173bf18be
Nikolai Orenstrach <nikoren@gmail.com>
2015-09-27 15:30:36 -0400

    Addeed  message handling for callback functions in reciever to simulate remote task listener that executes long running tasks , this way we can run several tasks and the queue will route the messages round robin

--- PikaExamples/first_App_receiver.py
+++ PikaExamples/first_App_receiver.py
@@ -1,6 +1,8 @@
-__author__ = 'niko'
-import pika
 
+from datetime import datetime
+__author__ = 'niko'
+import pika ,time
+import sys
 #define connection parameters
 my_credentials= pika.PlainCredentials(username='rmq_root',password='112233')
 parameters=pika.ConnectionParameters(host='192.168.122.2',credentials=my_credentials)
@@ -13,7 +15,23 @@ channel=connection.channel()
 #define calback functions to activate whenever new messages arrive
 def callback(channel,method,properties,body):
     print("[x] Recieved:  {}".format(body))
+    if body == 'EXIT':
+        print "Exiting .. "
+        sys.exit()
+    print "{} Executing job , it will take approximately {} seconds to complete".format(datetime.now(),body.count('.'))
+    time.sleep(body.count('.'))
+    print "[x] {} finished executing the task".format(datetime.now())
 
 print ' [*] Waiting for messages. To exit press CTRL+C'
 channel.basic_consume(consumer_callback=callback,queue='hello',no_ack=True)
 channel.start_consuming()
+
+
+# 1.................................................
+# 2.................................................
+# 3................................................
+# 4................................................
+# 5................................................
+# 6................................................
+# 7................................................
+# 8................................................
